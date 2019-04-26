092400af1fe9ce14b0f5fa9f5d6c94f125553917
Florian Brunner <fbrunnerreg@gmx.ch>
2019-04-27 00:11:48 +0200

    initial import

---
+++ drombler-photo-center-web/src/main/java/org/drombler/photo/center/web/DromblerPhotoCenterWebApplication.java
@@ -0,0 +1,13 @@
+package org.drombler.photo.center.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DromblerPhotoCenterWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DromblerPhotoCenterWebApplication.class, args);
+	}
+
+}
